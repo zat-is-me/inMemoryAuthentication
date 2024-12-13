b123d315c905d70daed632c6b258932a6ebd7949
Tatek <tatek.ahmed443@gmail.com>
2024-12-13 09:48:48 -0600

    Same review

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # InMemory Authentication Using username and password
 This application is build on Spring Boot 3.2.3
 
+
+1. Create SecurityFilterChain Bean in Config class and implement filtration to secure the end points in this bean.
+
+        @Bean
+        SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        
+          httpSecurity.csrf(csrf -> csrf.disable())
+                       .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/api/v1/register").permitAll()
+                            .requestMatchers("/api/v1/myAccount").authenticated())
+                       .httpBasic(withDefaults());
+           
+                 return httpSecurity.build();
+           }
+
 1. Create InMemoryUserDetailsManager bean
 
      
--- src/main/java/com/tatek/inMemoryAuthentication/config/AuthenticationConfig.java
+++ src/main/java/com/tatek/inMemoryAuthentication/config/AuthenticationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -29,7 +30,7 @@ public class AuthenticationConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 //    Approach 2
